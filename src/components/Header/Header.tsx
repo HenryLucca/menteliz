4e620762d745dd7cc2b506cb42593c72981bddab
Henry Lucca <henryluccag@gmail.com>
2024-01-31 21:38:25 -0300

    refactor: change offset threshold and add new styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,13 +4,13 @@ import { HeartIcon } from "@/assets/icons";
 import useHasScrolled from "@/hooks/useHasScrolled";
 
 export default function Header() {
-  const { scrolled } = useHasScrolled({ offsetThreshold: 600 });
+  const { scrolled } = useHasScrolled({ offsetThreshold: 300 });
 
   const initialHeaderClasses =
     "sticky w-full top-0 z-[9999] bg-mBlue-200 min-h-16 p-4 flex flex-col gap-4 md:flex-row justify-between md:items-center text-mBlue-600";
 
   const headerClassesAfterScroll =
-    "sticky w-full top-0 z-[9999] bg-white min-h-16 p-4 flex flex-col gap-4 md:flex-row justify-between md:items-center text-zinc-900";
+    "sticky w-full top-0 z-[9999] bg-white min-h-16 p-4 flex flex-col gap-4 md:flex-row justify-between md:items-center text-zinc-900  shadow-md";
 
   return (
     <header
