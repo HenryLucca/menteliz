5db6418a1b13e0976f504057c02c4f56c8162c50
Henry Lucca <henryluccag@gmail.com>
2024-01-31 22:54:20 -0300

    feat: add new icons: PillIcon and SmileIcon

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -25,3 +25,46 @@ export const HeartIcon = (props: IconProps) => {
     </svg>
   );
 };
+
+export const PillIcon = (props: IconProps) => {
+  const { size = 6, color = "#000", className } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      height={size}
+      width={size}
+      className={className}
+    >
+      <path d="m10.5 20.5 10-10a4.95 4.95 0 1 0-7-7l-10 10a4.95 4.95 0 1 0 7 7Z" />
+      <path d="m8.5 8.5 7 7" />
+    </svg>
+  );
+};
+
+export const SmileIcon = (props: IconProps) => {
+  const { size = 6, color = "#000", className } = props;
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      height={size}
+      width={size}
+      className={className}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+      />
+    </svg>
+  );
+};
