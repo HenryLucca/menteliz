81b9e1917c441680dd392ba3ef880e139128e332
Henry Lucca <henryluccag@gmail.com>
2024-02-01 14:22:25 -0300

    feat (wip): add FirstEditForm component. TODO: style

---
+++ src/app/perfil/FirstEditForm.tsx
@@ -0,0 +1,127 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
+import { useUserDataContext } from "@/contexts/UserDataContext";
+
+const FormSchema = z.object({
+  type: z.enum(["patients", "family_members", "doctors"], {
+    required_error: "Selecione o seu tipo de usuário.",
+  }),
+  userName: z.string().nonempty({
+    message: "Por favor, insira seu nome.",
+  }),
+});
+
+export default function FirstEditForm() {
+  const { createUser } = useUserDataContext();
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data);
+    if (createUser) {
+      createUser(data.type, data.userName);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="w-2/3 space-y-6">
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => (
+            <FormItem className="space-y-3">
+              <FormLabel>Selecione seu tipo de usuário</FormLabel>
+              <FormControl>
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex flex-col space-y-1"
+                >
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="patients" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Paciente</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="family_members" />
+                    </FormControl>
+                    <FormLabel className="font-normal">Familiar</FormLabel>
+                  </FormItem>
+                  <FormItem className="flex items-center space-x-3 space-y-0">
+                    <FormControl>
+                      <RadioGroupItem value="doctors" />
+                    </FormControl>
+                    <FormLabel className="font-normal">
+                      Médico/Terapeuta
+                    </FormLabel>
+                  </FormItem>
+                </RadioGroup>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="userName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="md:text-xl">Nome</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="seu@email.com"
+                  {...field}
+                  className="md:text-xl"
+                />
+              </FormControl>
+              <FormDescription className="md:text-xl">
+                Insira seu nome
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        {/* <FormField
+          control={form.control}
+          name="userName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <input
+                  type="text"
+                  {...field}
+                  className="w-full p-2 border rounded-md"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        /> */}
+        <Button type="submit">Salvar</Button>
+      </form>
+    </Form>
+  );
+}
