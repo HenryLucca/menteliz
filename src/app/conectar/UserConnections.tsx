ad381c5aca1b6681dc17ccdecf177804ae48af2a
HenryLucca <henryluccadev@gmail.com>
2024-02-13 11:20:17 -0300

    wip: create Conectar page and add SearchConnections

---
+++ src/app/conectar/UserConnections.tsx
@@ -0,0 +1,3 @@
+export default function UserConnections() {
+  return <section></section>;
+}
---
+++ src/app/conectar/page.tsx
@@ -0,0 +1,13 @@
+import Header from "@/components/Header/Header";
+import SearchConnections from "./SearchConnections";
+
+export default function Conectar() {
+  return (
+    <>
+      <Header />
+      <main className="bg-mBlue-200">
+        <SearchConnections />
+      </main>
+    </>
+  );
+}
