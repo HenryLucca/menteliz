e56d856ce867d456a30e4625a67006133a1aca38
Henry Lucca <henryluccag@gmail.com>
2024-02-25 14:26:23 -0300

    refactor: PendentConnections component and add ConnectionRequest component

--- src/app/conectar/PendentConnections.tsx
+++ src/app/conectar/PendentConnections.tsx
@@ -3,52 +3,41 @@
 import { useUserDataContext } from "@/contexts/UserDataContext";
 import { User } from "@/models/User";
 import { useEffect, useState } from "react";
+import ConnectionRequest from "./ConnectionRequest";
 
 export default function PendentConnections() {
   const { checkConnectionRequests, searchUserById } = useUserDataContext();
-  const [requests, setRequests] = useState([]);
+  const [requests, setRequests] = useState<any[]>([]);
   const [users, setUsers] = useState<User[]>([]);
-
-  console.log("requests- ", requests);
 
   useEffect(() => {
     const fetchRequests = async () => {
       if (checkConnectionRequests) {
-        const data = await checkConnectionRequests();
-        console.log("data- ", data);
-        setRequests(data);
+        const requestsData = await checkConnectionRequests();
+        console.log("requestsData- ", requestsData);
+        setRequests(requestsData);
+
+        if (requestsData != null && requestsData.length > 0 && searchUserById) {
+          const userRequests = await Promise.all(
+            requestsData.map((request: any) => searchUserById(request.senderId))
+          );
+
+          setUsers(userRequests);
+        }
       }
     };
 
     fetchRequests();
   }, [checkConnectionRequests, searchUserById]);
 
-  useEffect(() => {
-    const fetchUsers = async () => {
-      if (requests.length > 0) {
-        const users = await Promise.all(
-          requests.map(async (request: any) => {
-            if (searchUserById) {
-              const user = await searchUserById(request.from_user_id);
-              return user;
-            }
-          })
-        );
-        setUsers(users);
-      }
-    };
-
-    fetchUsers();
-  }, [requests, searchUserById]);
-
   return (
-    <section>
-      {requests?.length > 0 ? (
-        <div>
+    <section className="space-y-4">
+      {requests ? (
+        <div className="flex flex-col items-center justify-center">
           <h1>{requests.length} Conexões Pendentes</h1>
           <ul>
-            {users?.map((user: User, index: number) => {
-              return <li key={index}>{user.username}</li>;
+            {users.map((user: User, index: number) => {
+              return <ConnectionRequest key={index} user={user} />;
             })}
           </ul>
         </div>
