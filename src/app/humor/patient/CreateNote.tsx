61cf6234b1cf389bd64f175ab4210ef855531a6b
Henry Lucca <henryluccag@gmail.com>
2024-03-06 14:59:44 -0300

    feat: add CreateNote component

---
+++ src/app/humor/patient/CreateNote.tsx
@@ -0,0 +1,20 @@
+import CreateNoteForm from "./CreateNoteForm";
+
+export default function CreateNotes() {
+  return (
+    <section className="flex flex-col gap-6">
+      <div>
+        <h1 className="text-3xl font-bold tracking-tighter">
+          Rastreador de Humor
+        </h1>
+        <p className="text-gray-500">
+          Adicione novas anotações de como você está se sentindo.
+        </p>
+      </div>
+
+      <div className="px-4">
+        <CreateNoteForm />
+      </div>
+    </section>
+  );
+}
