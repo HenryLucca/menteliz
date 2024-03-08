e726bae1f41f9d9ba25a75c2637fb391c7009efb
HenryLucca <henryluccadev@gmail.com>
2024-03-08 11:42:20 -0300

    refactor: PatientPage component to use main tag

--- src/app/humor/patient/PatientPage.tsx
+++ src/app/humor/patient/PatientPage.tsx
@@ -6,9 +6,9 @@ import { Patient } from "@/models/User";
 export default function PatientHumor() {
   const { userData } = useUserDataContext();
   return (
-    <>
+    <main className="p-12 sm:p-16 space-y-12">
       <CreateNotes />
       <PatientNotes patient={userData as Patient} />
-    </>
+    </main>
   );
 }
