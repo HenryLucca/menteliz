90f004078c7dd31a6b1b48688764c0c429938f08
HenryLucca <henryluccadev@gmail.com>
2024-03-08 11:41:29 -0300

    feat: add Footer component

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="py-6 px-4 border-t w-full shrink-0 bg-mBlue-200">
+      <div className="text-center">
+        &copy; 2024 Mentaliz. Todos os direitos reservados.
+      </div>
+    </footer>
+  );
+}
