1e6a5cc3afc0eb9f140eed1c197415f9770f1a15
Henry Lucca <henryluccag@gmail.com>
2024-03-09 19:18:54 -0300

    fix typo

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="py-6 px-4 border-t w-full shrink-0 bg-mBlue-200">
       <div className="text-center">
-        &copy; 2024 Mentaliz. Todos os direitos reservados.
+        &copy; 2024 Menteliz. Todos os direitos reservados.
       </div>
     </footer>
   );
