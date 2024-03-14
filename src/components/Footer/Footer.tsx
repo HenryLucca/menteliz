84ea6e4cee1660eedcf026789438b84572e55c65
HenryLucca <henryluccadev@gmail.com>
2024-03-14 10:13:41 -0300

    feat: add NotFound component and update button styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="py-6 px-4 border-t w-full shrink-0 bg-mBlue-200">
-      <div className="text-center">
+      <div className="text-center text-gray-500">
         &copy; 2024 Menteliz. Todos os direitos reservados.
       </div>
     </footer>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,12 +5,12 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-zinc-950 dark:focus-visible:ring-zinc-300",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-white transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-950 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
-          "bg-zinc-900 text-zinc-50 hover:bg-zinc-900/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
+          "bg-mBlue-400 text-zinc-50 hover:bg-mBlue-600/90 dark:bg-zinc-50 dark:text-zinc-900 dark:hover:bg-zinc-50/90",
         destructive:
           "bg-red-500 text-zinc-50 hover:bg-red-500/90 dark:bg-red-900 dark:text-zinc-50 dark:hover:bg-red-900/90",
         outline:
