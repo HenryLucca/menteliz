24610211dde1d12cfd9884669b6fb83d527e957c
Henry Lucca <henryluccag@gmail.com>
2024-04-20 14:08:44 -0300

    feat: add layout with a BlogContextProvider wrapper

---
+++ src/app/blog/layout.tsx
@@ -0,0 +1,13 @@
+import BlogDataProvider from "@/contexts/BlogContext";
+
+interface BlogLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: BlogLayoutProps) {
+  return (
+    <>
+      <BlogDataProvider>{children}</BlogDataProvider>;
+    </>
+  );
+}
