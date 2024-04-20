52537fa8a4e12aeb2c550ec69c2b8dcb53706379
Henry Lucca <henryluccag@gmail.com>
2024-04-20 14:08:59 -0300

    refactor: blog page to use BlogContext

--- src/app/blog/new/page.tsx
+++ src/app/blog/new/page.tsx
@@ -2,21 +2,39 @@
 
 import Editor from "@/components/Editor/Editor";
 import { Button } from "@/components/ui/button";
+import { useBlogContext } from "@/contexts/BlogContext";
+import { useUserDataContext } from "@/contexts/UserDataContext";
 import useEditor from "@/hooks/useEditor";
+import { useRouter } from "next/navigation";
 
 export default function CreatePostPage() {
+  const { userData } = useUserDataContext();
+  const { createBlogPost } = useBlogContext();
+
+  const router = useRouter();
   const { editor } = useEditor();
 
-  function handleSave() {
+  async function handleSave() {
     // save post
-    console.log(editor?.getHTML());
+    const editorContent = editor?.getHTML();
+
+    if (editorContent && createBlogPost) {
+      console.log(editorContent, userData?.id);
+      const post = await createBlogPost(editorContent, userData?.id);
+
+      console.log(post);
+      const postId = post.post_id;
+      editor?.destroy();
+    }
   }
 
   return (
     <main className="px-[10%] py-8 lg:min-h-dvh bg-zinc-100">
       <div className="space-y-4">
         <h1 className="text-3xl font-bold ">Criar Post</h1>
-        <Editor />
+        <div>
+          <Editor />
+        </div>
         <Button
           onClick={handleSave}
           className="w-1/2 self-end justify-self-end"
