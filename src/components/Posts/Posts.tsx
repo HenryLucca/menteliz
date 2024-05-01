1e91cf16b62d9171b70626ff3ff506690bd800f6
Henry Lucca <henryluccag@gmail.com>
2024-05-01 20:12:44 -0300

    refactor: Posts component to use BlogContext for fetching and rendering blog posts

--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,22 +1,32 @@
 "use client";
 
 import { useUserDataContext } from "@/contexts/UserDataContext";
+import { useBlogContext } from "@/contexts/BlogContext";
 import { Button } from "../ui/button";
+import { useEffect } from "react";
 
 export default function Posts() {
   const { userData } = useUserDataContext();
+  const { posts, listPosts } = useBlogContext();
+
+  useEffect(() => {
+    listPosts && listPosts();
+  });
 
   return (
-    <div className="flex items-center">
-      <div>
-        <h1>Posts</h1>
-      </div>
-
+    <div className="">
       {userData?.type === "doctors" && (
         <a href="/blog/new">
           <Button variant={"link"}>Novo Post</Button>
         </a>
       )}
+      <div>
+        {posts?.map((post) => (
+          <div key={post.id} className="border p-4 my-4">
+            <h2>{post.title}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
